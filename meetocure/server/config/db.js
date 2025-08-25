898141d78c2459b6dff5e2b2b6a8d909f575478e
syamkumar <syamkumarchitti@gmail.com>
2025-08-25 08:23:15 +0530

    modify

--- meetocure/server/config/db.js
+++ meetocure/server/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
     try
     {
-        await mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/meetOCure");
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("CONNECTION OF MONGODB SUCCESSFUL !!");
     }
     catch(err)
